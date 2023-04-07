2c367cecc0d63fb559355ccaab586c5406cfdb72
edubsGit <121900778+edubsGit@users.noreply.github.com>
2023-04-07 12:42:01 -0700

    added mongodb login

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,61 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { compare } from 'bcrypt'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { prisma } from "@/lib/prisma/prisma"
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Sign in',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'hello@example.com'
+        },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (!user) {
+          return null
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          user.password
+        )
+
+        if (!isPasswordValid) {
+          return null
+        }
+
+        return {
+          id: user.id + '',
+          email: user.email,
+          name: user.name,
+          randomKey: 'Hey cool'
+        }
+      }
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
